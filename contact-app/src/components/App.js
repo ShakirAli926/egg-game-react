edb7ba5e70b1153a0051748836a54aac9ca45971
ShakirAli926 <shakirbugti926@gmail.com>
2022-05-15 19:09:56 +0500

    make game two cards

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,47 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
 import "./App.css";
-import Header from "./Header";
-import AddContact from "./AddContact";
-import ContactList from "./ContactList";
-import TodoList from "./TodoList";
+import Game from "./Game";
 
 function App() {
-  const LOCAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState([]);
 
-  const addContactHandler = (contact) => {
-    console.log(contact);
-    setContacts([...contacts, { id: uuidv4(), ...contact }]);
-  };
-
-  const removeContactHandler = (id) => {
-    const newContactList = contacts.filter((contact) => {
-      return contact.id !== id;
-    });
-
-    setContacts(newContactList);
-  };
-
-  useEffect(() => {
-    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    console.log(retriveContacts);
-    if (retriveContacts) setContacts(retriveContacts);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
-  }, [contacts]);
-
-  return (
-    <div className="ui container">
-      <Header />
-      <AddContact addContactHandler={addContactHandler} />
-      <ContactList contact={contacts} getContactid={removeContactHandler} />
-      <TodoList />
-    </div>
-  );
+    return (
+        <div className="ui container">
+            <div className="row justify-content-center d-flex">
+                    <Game userName={"You"} userScore={0}/>
+                    <Game userName={"John"} userScore={0}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
